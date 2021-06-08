1931d158c183682f61e03f3541dc2bc186128b9a
dudi13 <skladzia.ja@gmail.com>
2021-06-08 14:49:19 +0200

    2021-06-08 PD

--- app/models.py
+++ app/models.py
@@ -56,6 +56,19 @@ class Product:
     def export_to_json(self):
         with open(f"app/products/{self.product_id}.json", "w", encoding="UTF-8") as jf:
             json.dump(self.to_dict(), jf, ensure_ascii=False, indent=4)
+
+    def import_from_json(self):
+        with open(f"app/products/{self.product_id}.json", "r", encoding="UTF-8") as jf:
+            product = json.load(jf)
+        self.product_id = product['product_id']
+        self.product_name = product['product_name']
+        self.opinions_count = product['opinions_count']
+        self.pros_count = product['pros_count']
+        self.cons_count = product['cons_count']
+        self.average_score = product['average_score']
+        opinions = product['opinions']
+        for opinion in opinions:
+            self.opinions.append(Opinion(**opinion))
     
     def analyze(self):
         self.opinions_count = len(self.opinions)
@@ -77,7 +90,7 @@ class Opinion:
         "cons": ["div.review-feature__col:has(> div[class$=\"negatives\"]) > div.review-feature__item",
                  None,
                  True],
-        "verfied": ["div.review-pz"],
+        "verified": ["div.review-pz"],
         "post_date": ["span.user-post__published > time:nth-child(1)", "datetime"],
         "purchase_date": ["span.user-post__published > time:nth-child(2)", "datetime"],
         "usefulness": ["span[id^='votes-yes']"],
@@ -86,6 +99,17 @@ class Opinion:
 
     def __init__(self, opinion_id=None, author=None, recommendation=None, stars=None, content=None, pros=None, cons=None, verified=None, post_date=None, purchase_date=None, usefulness=None, uselessness=None) -> None:
         self.opinion_id = opinion_id
+        self.author = author
+        self.recommendation = recommendation
+        self.stars = stars
+        self.content = content
+        self.pros = pros
+        self.cons = cons
+        self.verified = verified
+        self.post_date = post_date
+        self.purchase_date = purchase_date
+        self.usefulness = usefulness
+        self.uselessness = uselessness
 
     def extract_components(self, opinion):
         for key, value in self.selectors.items():
@@ -97,7 +121,7 @@ class Opinion:
         self.recommendation = True if self.recommendation == "Polecam" else False if self.recommendation == "Nie polecam" else None
         self.stars = float(self.stars.split("/")[0].replace(",", "."))
         self.content = re.sub("\\s", " ", self.content)
-        self.verfied = bool(self.verfied)
+        self.verified = bool(self.verified)
         self.usefulness=int(self.usefulness)
         self.uselessness = int(self.uselessness)
         return self
